2dc27bb895561ef09c1f6009210c458a40e7739a
Emmanuel Mora <emmanuelmora05@gmail.com>
2017-06-25 17:00:15 -0600

    Add update action params to registration controller.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -21,9 +21,9 @@ class Users::RegistrationsController < DeviseTokenAuth::RegistrationsController
   # end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    super
+  end
 
   # DELETE /resource
   # def destroy
@@ -48,9 +48,10 @@ class Users::RegistrationsController < DeviseTokenAuth::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.for(:account_update) << :attribute
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :image
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
