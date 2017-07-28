a3205e80bae0701ecaacd98f5e8b4c34be59179c
Emmanuel Mora <emmanuelmora05@gmail.com>
2017-07-27 18:42:31 -0600

    Add firebase registration token to sign_up params filter.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:avatar, :firebase_registration_token])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar, :firebase_registration_token])
   end
 
