5e76b0ba8256b1da860bc3d4b877549b7dfc77a3
Emmanuel Mora <emmanuelmora05@gmail.com>
2017-08-03 19:37:42 -0600

    Add avatar url as data field in notifications.

--- app/services/firebase_cloud_messaging.rb
+++ app/services/firebase_cloud_messaging.rb
@@ -7,10 +7,17 @@ class FirebaseCloudMessaging
   # @param [Report] report
   # @param [Array<User>] to
   def self.send_new_report_notification(to, report)
+
+    if report.location_name.nil?
+      message = "Reportó un incendio"
+    else
+      message = "Reportó un incendio en #{report.location_name}"
+    end
+
     NotificationsJob.perform_later(to.map{|u| u.id}, {
         title: report.author.name,
-        body: "Reportó un incendio en #{report.location_name}"
-    })
+        body: message
+    },{ avatar: report.author.avatar.url })
   end
 
 end
--- app/workers/notifications_job.rb
+++ app/workers/notifications_job.rb
@@ -4,7 +4,7 @@ class NotificationsJob < ActiveJob::Base
   require 'uri'
   require 'json'
 
-  def send_push_notification(to,message)
+  def send_push_notification(to,message, data)
     uri = URI.parse("https://fcm.googleapis.com/fcm/send")
     http = Net::HTTP.new(uri.host,uri.port)
     http.use_ssl =true
@@ -14,6 +14,7 @@ class NotificationsJob < ActiveJob::Base
     request['Authorization'] = 'key=AAAAorGop6w:APA91bHOvtkg8P9Hyfge08N_jl5hJN2iT7ba1-8qhLFFYY6XbMmpTmF-lwLj9e7pWVh_3b4Kom5XcGsFOEOHHLfaxMWw2BZbHmevDgJnMG_IOj0bg3xJ_50hzQWIFQ4259MzXtk03Kww'
     request.body = {
         notification: message,
+        data: data,
         to: to
     }.to_json
 
@@ -27,7 +28,7 @@ class NotificationsJob < ActiveJob::Base
   def perform(users,message)
       users.map{ |id| User.find(id)  }.each do |user|
         if user.is_device_registered?
-          send_push_notification(user.firebase_registration_token, message)
+          send_push_notification(user.firebase_registration_token, message, data)
         end
       end
   end
