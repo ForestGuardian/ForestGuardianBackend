0cd8ad59dc44bab11459f546d317ba84025ddb3b
Emmanuel Mora <emmanuelmora05@gmail.com>
2017-08-04 20:02:02 -0600

    Add users show method.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'users', skip: [:omniauth_callbacks]
+      resources :users, only: [:show]
     end
   end
 
