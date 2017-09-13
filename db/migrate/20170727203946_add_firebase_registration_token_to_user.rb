13b0873be1b5fc0156a3f4c49c1c55daace0a44a
Emmanuel Mora <emmanuelmora05@gmail.com>
2017-09-13 15:30:59 -0600

    Remove migrations that are already in schema.rb

--- db/migrate/20170727203946_add_firebase_registration_token_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddFirebaseRegistrationTokenToUser < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :firebase_registration_token, :string
-  end
-end
--- db/migrate/20170730212522_add_location_name_to_reports.rb
+++
@@ -1,5 +0,0 @@
-class AddLocationNameToReports < ActiveRecord::Migration[5.0]
-  def change
-    add_column :reports, :location_name, :string
-  end
-end
