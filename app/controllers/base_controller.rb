9d4377b66b13bf27a51f41d58e52b36747434935
Emmanuel Mora <emmanuelmora05@gmail.com>
2017-09-13 18:15:20 -0600

    BaseController inherits ApplicationController.

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,4 +1,4 @@
-class BaseController < ActionController::Base
+class BaseController < ApplicationController
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
