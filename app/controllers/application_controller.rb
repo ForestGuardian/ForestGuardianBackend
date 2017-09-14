b746a4b18217402f9001d0b202194228415624e3
Emmanuel Mora <emmanuelmora05@gmail.com>
2017-09-13 18:03:22 -0600

    Add if json_request?.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,30 +1,29 @@
 class ApplicationController < ActionController::Base
 
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  # protect_from_forgery with: :exception
+  include ApplicationHelper
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+
+  respond_to :html,:json
+
   # # Check ActiveAdmin and DeviseAuthToken incompatibilities.
-  # unless controller_name.include? 'admin'
-    ::Rails.logger.warn "DEBUG: #{defined?(params) ? params : 'none'}"
-  #   include DeviseTokenAuth::Concerns::SetUserByToken
-  #   # Prevent CSRF attacks by raising an exception.
-  #   # For APIs, you may want to use :null_session instead.
-  #   # protect_from_forgery with: :exception
-  #   include ApplicationHelper
-  #
-  #   before_action :configure_permitted_parameters, if: :devise_controller?
-  #
-  #   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
-  #
-  #   respond_to :html,:json
-  #
-  #   def redirect_to(options = {}, response_status = {})
-  #     ::Rails.logger.error("Redirected by #{caller(1).first rescue "unknown"}")
-  #     super(options, response_status)
-  #   end
-  #
-  #   def configure_permitted_parameters
-  #     devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:avatar, :firebase_registration_token])
-  #     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar, :firebase_registration_token])
-  #   end
-  #
-  # end
+  include DeviseTokenAuth::Concerns::SetUserByToken  if json_request?
+
+  def redirect_to(options = {}, response_status = {})
+    ::Rails.logger.error("Redirected by #{caller(1).first rescue "unknown"}")
+    super(options, response_status)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:avatar, :firebase_registration_token])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar, :firebase_registration_token])
+  end
+
+end
 
 end
