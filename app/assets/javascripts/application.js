ce77055f0b9afe1f5fa4ee186f938542b820b7d7
Emmanuel Mora <emmanuelmora05@gmail.com>
2017-10-01 19:55:25 -0600

    Add change behaviour.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,27 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$("#agua").change(function () {
+    if (this.checked) {
+        showWindsLayer();
+    } else {
+        hideWindsLayer();
+    }
+})
+
+$("#lluvia").change(function () {
+    if (this.checked) {
+        showWeatherLayer();
+    } else {
+        hideWeatherLayer();
+    }
+})
+
+$("#bosque").change(function () {
+    if (this.checked) {
+        showForestLayer();
+    } else {
+        hideForestLayer();
+    }
+})
