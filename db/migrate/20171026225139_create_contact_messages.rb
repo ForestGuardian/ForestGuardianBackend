5a20ee00c4dce3688b16c425b80a2e28459c1faa
Emmanuel Mora <emmanuelmora05@gmail.com>
2017-10-26 17:46:26 -0600

    Add string and text fields in migration.

--- db/migrate/20171026225139_create_contact_messages.rb
+++ db/migrate/20171026225139_create_contact_messages.rb
@@ -2,6 +2,10 @@ class CreateContactMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :contact_messages do |t|
 
+      t.string :name
+      t.string :email
+      t.text :text
+
       t.timestamps
     end
   end
